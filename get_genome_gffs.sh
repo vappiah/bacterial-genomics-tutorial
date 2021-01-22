39c5f1d11efa461af4846f7d052eba4537f77078
vincentappiah <appiahv@rocketmail.com>
2021-01-22 02:09:16 +0000

    Update get_genome_gffs.sh

--- get_genome_gffs.sh
+++ get_genome_gffs.sh
@@ -2,13 +2,13 @@
 
 
 genomes=(genomes/*.fasta)
-mkidr gffs
+mkdir gffs
 
 for genome in Agy99 Liflandii SGL03 Shinsuense 
 do
 prokka --kingdom Bacteria --locustag ${genome} --addgenes --prefix ${genome} genomes/${genome}.fasta
 cp ${genome}/${genome}.gff gffs/
-rm -fr {genome}
+rm -fr ${genome}
 done
 
 cp P7741_annotation/P7741.gff gffs/
