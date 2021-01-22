54515d40a649976f5b3d43a03b624fd48ba05fbd
vincentappiah <appiahv@rocketmail.com>
2021-01-22 01:54:45 +0000

    Add files via upload

---
+++ get_genome_gffs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+genomes=(genomes/*.fasta)
+mkidr gffs
+
+for genome in Agy99 Liflandii SGL03 Shinsuense 
+do
+prokka --kingdom Bacteria --locustag ${genome} --addgenes --prefix ${genome} genomes/${genome}.fasta
+cp ${genome}/${genome}.gff gffs/
+rm -fr {genome}
+done
+
+cp P7741_annotation/P7741.gff gffs/
+
