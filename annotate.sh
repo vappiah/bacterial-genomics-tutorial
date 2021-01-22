490d589e49d102ef42dfb197c34218ca93840f2d
vincentappiah <appiahv@rocketmail.com>
2021-01-22 01:51:56 +0000

    Update annotate.sh

--- annotate.sh
+++ annotate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-prokka --kingdom Bacteria --locustag P7741 --prefix P7741 --addgenes P7741.reordered.fasta
-./get_pseudo.pl P7741/P7741.faa | tee P7741/P7741.pseudo.txt
+
+prokka --kingdom Bacteria --locustag P7741 --outdir P7741_annotation --prefix P7741 --addgenes P7741.reordered.fasta
+./get_pseudo.pl P7741_annotation/P7741.faa | tee P7741_annotation/P7741.pseudo.txt
