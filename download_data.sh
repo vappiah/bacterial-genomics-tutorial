5794c409cc6c6c2cf4ec5959e7e7068c361dec32
vincentappiah <appiahv@rocketmail.com>
2021-01-24 20:23:11 +0000

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -4,7 +4,7 @@
 mkdir data
 wget --no-check-certificate http://ftp.sra.ebi.ac.uk/vol1/run/ERR333/ERR3335404/P7741_R1.fastq.gz
 wget --no-check-certificate http://ftp.sra.ebi.ac.uk/vol1/run/ERR333/ERR3335404/P7741_R2.fastq.gz
-
+wget --no-check-certificate http://ftp.sra.ebi.ac.uk/vol1/run/ERR333/ERR3336325/all_minion.fastq.tar.gz
 
 
 #move reads to data directory
