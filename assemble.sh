8de1e26de35505079d44737170fd2a7856cb8e41
vincentappiah <appiahv@rocketmail.com>
2021-01-25 03:49:30 +0000

    Update assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -3,7 +3,6 @@
 #DE NOVO ASSEMBLY WITH SPADES
 #THIS WORKFLOW USES ILLUMINA AND NANOPORE READS
 
-#
-
-spades.py --isolate -o P7741_SPADES_OUT -1 trimmed_reads/P7741_R1.fastq -2 trimmed_reads/P7741_R2.fastq -s trimmed_reads/P7741_S.fastq --nanopore trimmed_reads/P7741_minion.fastq
+#--nanopore trimmed_reads/P7741_minion.fastq
+spades.py --isolate -o P7741_SPADES_OUT -1 trimmed_reads/P7741_R1.fastq -2 trimmed_reads/P7741_R2.fastq -s trimmed_reads/P7741_S.fastq 
 
