ded3068d29e97856da41d4151e560f54e8249dc9
vincentappiah <appiahv@rocketmail.com>
2021-01-25 03:53:03 +0000

    Update trim_reads.sh

--- trim_reads.sh
+++ trim_reads.sh
@@ -8,7 +8,7 @@
 mkdir trimmed_reads
 
 #trim illumina reads using sickle
-sickle pe -f data/P7741_R1.fastq.gz -r data/P7741_R2.fastq.gz -t sanger -o trimmed_reads/P7741_R1.fastq -p trimmed_reads/P7741_R2.fastq -s trimmed_reads/P7741_S.fastq -q 20 -l 20
+sickle pe -f data/P7741_R1.fastq.gz -g -r data/P7741_R2.fastq.gz -t sanger -o trimmed_reads/P7741_R1.fastq -p trimmed_reads/P7741_R2.fastq -s trimmed_reads/P7741_S.fastq -q 20 -l 20
 
 
 
