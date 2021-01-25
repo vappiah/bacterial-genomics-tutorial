f4e7fee55dee78bb6a803ba640c6ddcd03f91926
vincentappiah <appiahv@rocketmail.com>
2021-01-25 01:05:04 +0000

    Update assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -5,6 +5,6 @@
 #THERE IS NO SINGLE READS AVAILABLE FOR THIS SO THE -S FLAG IS OMMITTED
 
 
-spades.py --isolate -o P7741_SPADES_OUT -1 data/P7741_R1.fastq.gz -2 data/P7741_R2.fastq.gz  --nanopore data/all_minion.fastq.tar.gz
+spades.py --isolate -o P7741_SPADES_OUT -1 data/P7741_R1.fastq.gz -2 data/P7741_R2.fastq.gz  --nanopore data/P7741_minion.fastq
 
 
