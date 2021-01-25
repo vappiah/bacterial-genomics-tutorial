2b880d98c3a12ab4b8e28a02668349965ce34b38
vincentappiah <appiahv@rocketmail.com>
2021-01-25 11:57:41 +0000

    Add files via upload

---
+++ get_pangenome.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+threads=4
+
+#Identify pangenomes
+roary -f pangenome -p $threads -e -n -v --maft gffs/*.gff
+
+#Generate alignment file
+FastTree -nt -gtr pangenome/core_gene_alignment.aln > pangenome/mytree.newick
+
+#Plot phylogenetic tree and generate svg format
+python roary_plots.py --labels --format svg pangenome/mytree.newick pangenome/gene_presence_absence.csv
+
+#Plot phylogenetic tree and generate image in png format
+python roary_plots.py --labels pangenome/mytree.newick pangenome/gene_presence_absence.csv
+
+#mv all plots to pangenome folder
+mv pangenome_*.{svg,png} pangenome
+
+echo "Pangeome analysis complete"
+echo "Output: pangenome"
+ 
+
