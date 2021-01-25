a6e559efae83d423d546e0e9467a8d6c95f27e02
vincentappiah <appiahv@rocketmail.com>
2021-01-25 01:05:55 +0000

    Update assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #DE NOVO ASSEMBLY WITH SPADES
-#THIS WORKFLOW USES ILLUMINA DATA
-#THERE IS NO SINGLE READS AVAILABLE FOR THIS SO THE -S FLAG IS OMMITTED
+#THIS WORKFLOW USES ILLUMINA AND NANOPORE READS
 
 
-spades.py --isolate -o P7741_SPADES_OUT -1 data/P7741_R1.fastq.gz -2 data/P7741_R2.fastq.gz  --nanopore data/P7741_minion.fastq
+spades.py --isolate -o P7741_SPADES_OUT -1 data/P7741_R1.fastq.gz -2 data/P7741_R2.fastq.gz --nanopore data/P7741_minion.fastq
 
 
