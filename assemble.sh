1d03bbad45a92b968dbe26c6a46c67444eb954fc
vincentappiah <appiahv@rocketmail.com>
2021-01-25 02:44:10 +0000

    Update assemble.sh

--- assemble.sh
+++ assemble.sh
@@ -4,6 +4,6 @@
 #THIS WORKFLOW USES ILLUMINA AND NANOPORE READS
 
 
-spades.py --isolate -o P7741_SPADES_OUT -1 data/P7741_R1.fastq.gz -2 data/P7741_R2.fastq.gz --nanopore data/P7741_minion.fastq
 
+spades.py --isolate -o P7741_SPADES_OUT -1 trimmed_reads/P7741_R1.fastq -2 trimmed_reads/P7741_R2.fastq -s trimmed_reads/P7741_S.fastq --nanopore trimmed_reads/P7741_minion.fastq
 
