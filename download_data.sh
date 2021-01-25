c9ad133de6cc7bff93e91de04ea7350668081365
vincentappiah <appiahv@rocketmail.com>
2021-01-25 05:03:38 +0000

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,7 @@
 mkdir data
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR333/004/ERR3335404/ERR3335404_1.fastq.gz -O data/P7741_R1.fastq.gz
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR333/004/ERR3335404/ERR3335404_2.fastq.gz -O data/P7741_R2.fastq.gz
+wget https://sra-pub-src-1.s3.amazonaws.com/ERR3336325/all_minion.fastq.1 -O data/P7741_minion.fastq
 
 
 
