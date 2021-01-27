06b88ba9d0a86ffa969e34cd89bd6e7a36a124b2
vincentappiah <appiahv@rocketmail.com>
2021-01-27 09:31:37 +0000

    Update annotate.sh

--- annotate.sh
+++ annotate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#Genome annotation using prokka
 
+#if you have more cpus you can increase the number of cpus
+cpus=4
 
-prokka --kingdom Bacteria --locustag P7741 --outdir P7741_annotation --prefix P7741 --addgenes P7741.reordered.fasta
+prokka --cpus $cpus --kingdom Bacteria --locustag P7741 --outdir P7741_annotation --prefix P7741 --addgenes P7741.reordered.fasta
 ./get_pseudo.pl P7741_annotation/P7741.faa | tee P7741_annotation/P7741.pseudo.txt
