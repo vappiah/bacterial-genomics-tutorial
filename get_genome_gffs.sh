767b4c81c2db6edc10d4f71dc7d0f91add0567f8
vincentappiah <appiahv@rocketmail.com>
2021-01-27 09:28:30 +0000

    Update get_genome_gffs.sh

--- get_genome_gffs.sh
+++ get_genome_gffs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+#if you have more cpu cores you can increase the number of cpus
+cpus=4
 
 genomes=(genomes/*.fasta)
 mkdir gffs
 
 for genome in Agy99 Liflandii SGL03 Shinsuense H37Rv 
 do
-prokka --kingdom Bacteria --locustag ${genome} --addgenes --prefix ${genome} genomes/${genome}.fasta
+prokka --cpus $cpus --kingdom Bacteria --locustag ${genome} --addgenes --prefix ${genome} genomes/${genome}.fasta
 cp ${genome}/${genome}.gff gffs/
 rm -fr ${genome}
 done
