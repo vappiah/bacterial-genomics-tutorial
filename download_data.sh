307e840504df6f24175ff92ee5fd2de92dbe736d
vincentappiah <appiahv@rocketmail.com>
2023-03-28 12:30:47 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,9 @@
 mkdir data
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR333/004/ERR3335404/ERR3335404_1.fastq.gz -O data/P7741_R1.fastq.gz
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR333/004/ERR3335404/ERR3335404_2.fastq.gz -O data/P7741_R2.fastq.gz
+
+mkdir apps
+wget https://github.com/broadinstitute/pilon/releases/download/v1.24/pilon-1.24.jar -P apps/
 #wget https://sra-pub-src-1.s3.amazonaws.com/ERR3336325/all_minion.fastq.1 -O data/P7741_minion.fastq
 
 
