71b5081582cefe5ea5951c0c92fb4745634e0376
vincentappiah <appiahv@rocketmail.com>
2023-07-24 22:28:31 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,7 @@
 mkdir data
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR333/004/ERR3335404/ERR3335404_1.fastq.gz -O data/P7741_R1.fastq.gz
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR333/004/ERR3335404/ERR3335404_2.fastq.gz -O data/P7741_R2.fastq.gz
+wget https://raw.githubusercontent.com/timflutre/trimmomatic/master/adapters/TruSeq3-PE.fa
 
 mkdir apps
 wget https://github.com/broadinstitute/pilon/releases/download/v1.24/pilon-1.24.jar -O apps/pilon.jar
