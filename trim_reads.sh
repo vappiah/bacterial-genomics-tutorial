2dea05a1b75d3688f00a3044b524072d184923ae
vincentappiah <appiahv@rocketmail.com>
2023-07-24 22:33:09 +0200

    Update trim_reads.sh

--- trim_reads.sh
+++ trim_reads.sh
@@ -1,14 +1,18 @@
 ##!/bin/bash
 
-#trim nanopore reads using porechop
-#porechop -i data/P7741_minion.fastq -o trimmed_reads/P7741_minion.fastq --format fastq -t 4
-
-
-
 mkdir trimmed_reads
+read1=data/P7741_R1.fastq.gz
+read2=data/P7741_R2.fastq.gz
+OutputForwardPaired=trimmed_reads/P7741_R1.fastq.gz    
+OutputForwardUnpaired=trimmed_reads/P7741_unpaired_R1.fastq.gz
+OutputReversePaired=trimmed_reads/P7741_R2.fastq.gz
+OutputReverseUnpaired=trimmed_reads/P7741_unpaired_R2.fastq.gz
+threads=8
 
-#trim illumina reads using sickle
-sickle pe -g -f data/P7741_R1.fastq.gz -r data/P7741_R2.fastq.gz -t sanger -o trimmed_reads/P7741_R1.fastq.gz -p trimmed_reads/P7741_R2.fastq.gz -s trimmed_reads/P7741_S.fastq.gz -q 20 -l 20
+trimmomatic PE -threads $threads $read1 $read2 \
+$OutputForwardPaired $OutputForwardUnpaired \
+$OutputReversePaired $OutputReverseUnpaired \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:8:true HEADCROP:3 TRAILING:10 MINLEN:25
 
 
 #trim nanopore reads using porechop
