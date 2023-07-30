38840dd039e6b441c1bf35fc511621ff0a97b587
vincentappiah <appiahv@rocketmail.com>
2023-07-30 20:44:52 +0200

    Update dendogram.sh

--- dendogram.sh
+++ dendogram.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+#mkdir tmp_fastas
+#cp genomes/*.fasta tmp_fastas
+#cp P7741.reordered.fasta tmp_fastas/
+#mkdir dendogram
+#dRep compare dendogram -g tmp_fastas/*.fasta
+#rm -fr tmp_fastas
 
-mkdir tmp_fastas
-cp genomes/*.fasta tmp_fastas
-cp P7741.reordered.fasta tmp_fastas/
-mkdir dendogram
-dRep compare dendogram -g tmp_fastas/*.fasta
-rm -fr tmp_fastas
+#echo "dendogram generated"
+#echo "output: ./dendogram"
 
-echo "dendogram generated"
-echo "output: ./dendogram"
+
+echo "This script has been deactivated."
+echo "This is because of installation issues with fastANI"
+echo "To run the commands for this script, you have to "
+echo "1. Install fastANI"
+echo "2. Uncomment out the commands in this script"
+echo "Please continue with the rest of the analysis"
+
